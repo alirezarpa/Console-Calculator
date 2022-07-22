9e3cd093d7a682bc9331f80c68196f9ab15da6a1
Alireza Rahimipour Anaraki <108992976+alirezarpa@users.noreply.github.com>
2022-07-22 14:40:09 +0430

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,39 @@
+# By Alireza Rahimipour Anaraki
+# https://zil.ink/ARRahimipour
+
+import os
+os.system('cls')
+
+def calculate():
+    operation = input('''
+Please enter math operation:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+# Call calculate() outside of the function
+calculate()
